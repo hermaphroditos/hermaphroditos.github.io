ff7b0a8a269abf5e6a1250bd95f9ed5c81521338
Iz Jeon <ido23j@gmail.com>
2023-11-15 23:42:22 +0100

    add: Landing.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { NewNote } from './components/NewNote';
@@ -8,6 +8,8 @@ import Counter from './components/Counter';
 import List from './components/List';
 import useMediaQuery from './hooks/useMediaQuery';
 import Navbar from './components/Navbar';
+import DotGroup from './components/DotGroup';
+import Landing from './pages/Landing';
 
 const queryProps = {
   query: "(min-width: 1060px)"
@@ -17,6 +19,7 @@ function App() {
   
   // page selection
   const [selectedPage, setSelectedPage] = useState('home');
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
   const isAboveMediumScreens = useMediaQuery(queryProps);
   const pageProps = {
     page: 'home',
@@ -24,21 +27,39 @@ function App() {
     setSelectedPage: setSelectedPage
   };
 
+  // counter setup
   const [count, setCount] = useState<number>(1)
 
+  // to top of the webpage
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  
   return (
-    <div className="App bg-deep-blue">
-      <Container className="my-4">
-        <header className="App-header">
-          <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} /> {/* 수정 */}
-          <Heading title="Hello !"/>
-          <Counter setCount={setCount} >{count}</Counter>
-        </header>
-        <div>
-          <Section title='undertitle' children='children here' />
-          <List items={["coffee","tacos","code"]} render={(item:string) => <span>{item}</span>} />
-        </div>
-      </Container>
+    <div className="app bg-deep-blue">
+      <div className='app-header'>
+        <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} /> 
+      </div>
+      <div className="w-5/6 mx-auto md:h-full">
+        {/* <Section title='undertitle' children='children here' /> */}
+        {isAboveMediumScreens && (<DotGroup selectedPage={selectedPage} setSelectedPage={setSelectedPage} />)}
+        <Landing selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      </div>
+      <div className="w-5/6 mx-auto md:h-full">
+        <List items={["coffee","tacos","code"]} render={(item:string) => <span>{item}</span>} />
+      </div>
+      <footer>
+        <Heading title="Hello !"/>
+        <Counter setCount={setCount} >{count}</Counter>
+      </footer>
     </div>
   );
 }
