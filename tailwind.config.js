d9b1467f60115e8e0ee08a81032a3478ff8d5035
Iz Jeon <ido23j@gmail.com>
2023-11-15 23:40:57 +0100

    update: css settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
   mode: 'jit',
   theme: {
     extend: {
       colors: {
-        blue: '#2CBCE9',
-        red: '#DC4492',
         "deep-blue": "#010026",
-        'opaque-black': 'rbga(0,0,0,0,35) '
+        blue: "#2CBCE9",
+        red: "#DC4492",
+        yellow: "#FDCC49",
+        grey: "#ededed",
+        "dark-grey": "#757575",
+        "opaque-black": "rgba(0,0,0,0.35)",
       },
       backgroundImage: (theme) => ({
-        "gradient-rainbow": 
-        "linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.0%)",
-        "gradient-rainblue": 
-        "linear-gradient(90deg, #24CBFF 14.21%, #FC59FF 69.05%, #FFBD0C 78.0%)"
+        "gradient-rainbow":
+          "linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)",
+
+        "gradient-rainblue":
+          "linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)",
       }),
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+        opensans: ["Open Sans", "sans-serif"],
+      },
       content: {
-        brush: "url('./assets/brush.png')",
-        person1: "url(./assets/person-1.png')"
-      }
+      },
     },
     screens: {
       xs: "480px",
+      ss: "620px",
       sm: "768px",
-      md: "1060px"
-    }
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
 }
